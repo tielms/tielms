79fd10ce199093db430f65796ad48734a320dd17
TieLMS <info@tielms.com>
2022-12-03 22:42:11 +0100

    tielms initial commit

---
+++ public/themes/edugator/ThemeInfo.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'name'          => 'Edugator - Tielearn academy theme',
+    'description'   => 'Beautiful LMS theme that covers your needs, Powered By Tielearn academy',
+    'url'           => 'https://tielearn.com',
+    'author'        => 'tielearn',
+    'author_url'    => 'https://tielearn.com',
+    'version'       => '1.0.0',
+];
